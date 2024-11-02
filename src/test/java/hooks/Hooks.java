99d770efa346102ce91c4fa3310eb35075c91173
ashrar khan <kknhak07@gmail.com>
2024-11-03 00:53:21 +0530

    implementation of Hook class 2

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -1,5 +1,38 @@
 package hooks;
 
-public class Hooks {
+import java.io.IOException;
+import java.util.Properties;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebDriver;
+
+import factory.BaseClass;
+import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+
+public class Hooks {
+WebDriver driver;
+Properties p;
+
+@Before
+public void setup()throws IOException{
+	driver=BaseClass.initilizeBrowser();
+	p=BaseClass.getProperties();
+	driver.get(p.getProperty("appURL"));
+	driver.manage().window().maximize();
+}
+@After
+public void tearDown() {
+	driver.quit();
+}
+@AfterStep
+public void addScreenshot(Scenario scenario) {
+	if(scenario.isFailed()) {
+		TakeScreenshot ts=(TakeScreenshot)driver;
+		byte[] screenshot = ts.getScreenshotAs(OutputType.BYTES);
+		scenario.attach(screenshot,"image/png",scenario.getName());
+	}
+}
 }
